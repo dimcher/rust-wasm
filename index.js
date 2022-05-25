6b4979234fe2e9e78f1eeacd0d95399c5cc2177f
Dima Chernyshev <dimcher@gmail.com>
2022-05-25 15:58:57 -0400

    new files

---
+++ index.js
@@ -0,0 +1,3 @@
+const rust = import('./pkg/rust_3d_demo');
+
+rust.then(m => m.say_hello_from_rust());
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
+const path = require('path')
 const webpack = require('webpack');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = (env, args) => {
     const isProductionMode = (args.mode === 'production');
 
     return {
         entry: './index.js',
-        mode: 'production',
         output: {
-            path: path.resolve(__dirname, 'dist'),
+            path: path.resolve(__dirname, 'dist',)
             filename: isProductionMode ? '[name].[contenthash].js' : '[name].[hash].js'
         },
         plugins: [
@@ -18,7 +17,7 @@ module.exports = (env, args) => {
                 template: 'index.html'
             }),
             new WasmPackPlugin({
-                createDirectory: path.resolve(__dirname, '.')
+                crateDirectory: path.resolve(__dirname, '.')
             }),
             new webpack.ProvidePlugin({
                 TextDecoder: ['text-encoding', 'TextDecoder'],
